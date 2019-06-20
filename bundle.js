644bba83408014badc25c4ae0f80cc87de46d56a
Milan <milan.dhar@gmail.com>
2019-06-19 18:21:55 -0700

    got phone number working

--- bundle.js
+++ bundle.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 // const breakURL = 'http://localhost:3000/api/v1/breaks';
 // const accountSid = 'AC90f8b4c917046d37d6af128295b2fe4d';
 // const authToken = 'cb0191398a42c8c5531bbf4157ac32c1';
@@ -41,20 +42,20 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
 })
 
 
-// function sendMessage(){
-//   let message = ""
-//   chrome.storage.local.get('phone_number', function (result) {
-//     message = result.phone_number;
-//
-//
-//   client.messages.create({
-//   to: `+1${message}`,
-//   from: '+12406410870',
-//   body: 'This is a test for our break timer app!'
-// })
-//
-// })
-// }
+function sendMessage(){
+  let message = ""
+  chrome.storage.local.get('phone_number', function (result) {
+    message = result.phone_number;
+
+
+  client.messages.create({
+  to: `+1${message}`,
+  from: '+12406410870',
+  body: 'This is a test for our break timer app!'
+})
+
+})
+}
 
 function deactivateAlarm(){
 
@@ -101,3 +102,5 @@ function deactivateAlarm(){
 //     });
 //   }
 // });
+
+},{}]},{},[1]);
